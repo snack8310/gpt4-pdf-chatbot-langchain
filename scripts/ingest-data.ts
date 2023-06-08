b9a76a0c0d99447ae1c1db4b06db600a19b265ac
tanhai <tanhai@wacai.com>
2023-06-08 16:00:58 +0800

    change vector store to qdrant

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,10 +1,9 @@
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { pinecone } from '@/utils/pinecone-client';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
-import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import {RecursiveCharacterTextSplitter} from 'langchain/text_splitter';
+import {OpenAIEmbeddings} from 'langchain/embeddings/openai';
+import { QdrantVectorStore } from "langchain/vectorstores/qdrant";
+
+import {PDFLoader} from 'langchain/document_loaders/fs/pdf';
+import {DirectoryLoader} from 'langchain/document_loaders/fs/directory';
 
 /* Name of directory to retrieve your files from 
    Make sure to add your PDF files inside the 'docs' folder
@@ -12,42 +11,52 @@ import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 const filePath = 'docs';
 
 export const run = async () => {
-  try {
-    /*load raw docs from the all files in the directory */
-    const directoryLoader = new DirectoryLoader(filePath, {
-      '.pdf': (path) => new PDFLoader(path),
-    });
+    try {
+        /*load raw docs from the all files in the directory */
+        const directoryLoader = new DirectoryLoader(filePath, {
+            '.pdf': (path) => new PDFLoader(path),
+        });
 
-    // const loader = new PDFLoader(filePath);
-    const rawDocs = await directoryLoader.load();
+        // const loader = new PDFLoader(filePath);
+        const rawDocs = await directoryLoader.load();
 
-    /* Split text into chunks */
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
-    });
+        /* Split text into chunks */
+        const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 200,
+        });
 
-    const docs = await textSplitter.splitDocuments(rawDocs);
-    console.log('split docs', docs);
+        const docs = await textSplitter.splitDocuments(rawDocs);
+        console.log('split docs', docs);
 
-    console.log('creating vector store...');
-    /*create and store the embeddings in the vectorStore*/
-    const embeddings = new OpenAIEmbeddings();
-    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+        console.log('creating vector store...');
+        /*create and store the embeddings in the vectorStore*/
+        const embeddings = new OpenAIEmbeddings();
+        // const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
 
-    //embed the PDF documents
-    await PineconeStore.fromDocuments(docs, embeddings, {
-      pineconeIndex: index,
-      namespace: PINECONE_NAME_SPACE,
-      textKey: 'text',
-    });
-  } catch (error) {
-    console.log('error', error);
-    throw new Error('Failed to ingest your data');
-  }
+        //embed the PDF documents
+        // await PineconeStore.fromDocuments(docs, embeddings, {
+        //   pineconeIndex: index,
+        //   namespace: PINECONE_NAME_SPACE,
+        //   textKey: 'text',
+        // });
+
+        await QdrantVectorStore.fromDocuments(
+            docs,
+            new OpenAIEmbeddings(),
+            {
+                url: process.env.QDRANT_URL,
+                collectionName: "aaab_test_collection",
+            }
+        );
+
+    } catch (error) {
+        console.log('error', error);
+        throw new Error('Failed to ingest your data');
+    }
 };
 
 (async () => {
-  await run();
-  console.log('ingestion complete');
+    await run();
+    console.log('ingestion complete');
 })();
