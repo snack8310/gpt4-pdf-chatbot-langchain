7785e1cb7a77d3f3dcc93eaaca89b9c11ccc91da
zhushiyi <matthewweasley@163.com>
2023-06-16 18:15:33 +0800

    add new ui

---
+++ types/docs.ts
@@ -0,0 +1,13 @@
+export type Docs = {
+  id: string;
+  docs_name: string;
+  docs_path: string;
+  total_page: number;
+  remark: string;
+  active: boolean;
+  created_by: string;
+  updated_by: string;
+  created_time: string;
+  updated_time: string;
+
+}
